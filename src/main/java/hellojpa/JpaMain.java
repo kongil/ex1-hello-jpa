6a2a0c6df819d17cb4fb850dfffe84b7fe132788
kongil <o_o0122@naver.com>
2022-10-24 23:14:06 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,9 +15,11 @@ public class JpaMain {
         try {
 
             //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZ");
-//            em.persist(member);
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
             System.out.println("============================");
 
             tx.commit();
