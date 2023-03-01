7a05d0488f0852b2921220ebb789d155722848cd
kongil <o_o0122@naver.com>
2023-03-01 19:22:49 +0900

    필드와 컬럼 매핑(@Column, @Temporal, @Enumerated, @Lob, @Transient)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,15 +14,13 @@ public class JpaMain {
 
         try {
 
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-//            em.detach(member);
-//            em.clear();
-            Member member2 = em.find(Member.class, 150L);
+            em.persist(member);
 
-            System.out.println("============================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
