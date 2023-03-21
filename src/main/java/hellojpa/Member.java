d10252f33ded6da131296b4720dde30885d18181
kongil <o_o0122@naver.com>
2023-03-22 00:24:06 +0900

    임베디드 타입  - 새로운 값 타입 직접 정의  - 주로 기본 값 타입을 모아 만들어서 복합 값 타입이라고도 함  - @Embeddable : 값 타입을 정의하는 곳에 표시  - @Embedded : 값 타입을 사용하는 곳에 표시  - 기본 생성자 필수 (생성자 없는 경우는 default)  - 장점    재사용, 높은 응집도, 해당 값 타입만 사용하는 의미 있는 메소드  - 임베디드 타입을 사용하기 전과 후에 매핑하는 테이블은 같다  - 객체와 테이블을 아주 세밀하게(find-grained) 매핑하는 것이 가능  - 잘 설계한 ORM 애플리케이션은 클래스 수 > 테이블 수

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-public class Member extends BaseEntity{
+public class Member {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -16,17 +16,24 @@ public class Member extends BaseEntity{
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
-    private Team team;
+    //기간 Period
+    @Embedded
+    private Period workPeriod;
 
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
+    //주소
+    @Embedded
+    private Address homeAddress;
+    //주소
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city",
+                    column =@Column(name="WORK_CITY")),
+            @AttributeOverride(name="street",
+                    column = @Column(name="WORK_STREET")),
+            @AttributeOverride(name="zipcode",
+                    column = @Column(name="WORK_ZIPCODE"))
+    } )
+    private Address workAddress;
 
     public Long getId() {
         return id;
@@ -44,5 +51,20 @@ public class Member extends BaseEntity{
         this.username = username;
     }
 
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
 }
 
