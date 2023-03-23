6e24539a015f59440425bcd5782ce7849ff0c0c3
kongil <o_o0122@naver.com>
2023-03-23 21:16:59 +0900

    값 타입 컬렉션  - @ElementCollection @CollectionTable(name = , joinColumns =) 사용  - 데이터베이스는 컬렉션을 같은 테이블에 저장할 수 없다. 그래서 일대다 개념으로 풀어서 별도의 테이블에 관리해야함  - 값 타입 저장/조회(지연 로딩 전략 사용)/수정  - 값 타입 컬렉션은 영속성 전이(Cascade) + 고아객체 제거 기능(orphanRemoval)
     *값 타입 컬렉션의 제약
      - 값 타입 컬렉션에 변경 사항이 발생하면 주인 엔티티와 연관된 모든 데이터 삭제하고 컬렉션 데이터 삽입
      - 값 타입 컬렉션을 매핑하는 테이블은 모든 컬럼을 묶어 기본키 구성 : null 입력x, 중복 저장x
      ==> 실무에서 완전 간단한 경우를 제외하고 값 타입 컬렉션 대신 일대다 관계를 고려,
          일대다 관계를 위한 엔티티를 만들고 여기서 값 타입 사용,
          영속성 전이(Cascade) + 고아 객체 제거를 사용해 값 타입 컬렉션 처럼 사용
    
     엔티티 vs 값 타입 특징 차이
     식별자o | 식별자x
     생명주기관리 | 생명주기 의존
     공유o | 공유하지 않는 것이 안전, 즉 불변 객체가 안전

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,9 +2,7 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 public class Member {
@@ -16,13 +14,26 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    //기간 Period
-    @Embedded
-    private Period workPeriod;
-
     //주소
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME") // 값이 하나고 내가 정의하지 않았기에 예외적으로 가능
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//            @JoinColumn(name = "MEMBER_ID")
+//    )
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -40,20 +51,28 @@ public class Member {
         this.username = username;
     }
 
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
-
     public Address getHomeAddress() {
         return homeAddress;
     }
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
 
